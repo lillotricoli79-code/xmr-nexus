50fcf11f1d18aa493f775754baea64c8efb220df
lillotricoli79-code <lillotricoli79@gmail.com>
2025-11-13 21:36:49 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,1 @@
 
-
-
-function setPool() {
-  const pool = document.getElementById("poolSelect").value;
-  document.getElementById("pool").textContent = pool;
-  alert("Pool cambiato in: " + pool);
-}
-
-// Simulazione hash rate (demo)
-setInterval(() => {
-  const randomHashrate = (Math.random() * 1000).toFixed(2);
-  document.getElementById("hashrate").textContent = randomHashrate + " H/s";
-}, 2000);
